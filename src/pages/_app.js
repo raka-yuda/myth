ef5aea478fb2ce915a6452a0d69c04dd17e9e5de
Raka Yuda Pradipta <raka.yuda.pradipta@gmail.com>
2024-08-06 22:29:20 +0700

    feat: add loading page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,29 @@
+import Loading from "@/components/Loading";
 import "@/styles/globals.css";
+import { useEffect, useState } from "react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 1500); // Set the same duration as the Loading component
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>Myth</title>
+      </Head>
+      {isLoading ? (
+        <Loading text="Welcome to the Mythical World!" duration={1000}/>
+      ) : (
+        <Component {...pageProps} />
+      )}
+    </>
+  );
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>Myth</title>
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🗿</text></svg>"/>
         {/* <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
