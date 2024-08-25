244e3f08293dc4a9a055214e70b947e21f4f7742
Raka Yuda Pradipta <raka.yuda.pradipta@gmail.com>
2024-08-25 15:18:38 +0700

    feat: add quiz page test

---
+++ src/pages/__tests__/quiz.test.js
@@ -0,0 +1,25 @@
+import { render, screen } from '@testing-library/react'
+import QuizPage from '../quiz'
+import { useRouter } from 'next/router';
+
+// Mock the useRouter hook
+jest.mock('next/router', () => ({
+  useRouter: jest.fn(),
+}));
+
+describe('Home', () => {
+  it('renders a heading', () => {
+    const mockRouter = {
+      pathname: '/',
+      route: '/',
+      query: {},
+      asPath: '/',
+    };
+  
+    useRouter.mockImplementation(() => mockRouter);
+    
+    render(<QuizPage />)
+    const heading = screen.getByRole('heading', { name: /Quiz/i })
+    expect(heading).toBeInTheDocument()
+  })
+})
