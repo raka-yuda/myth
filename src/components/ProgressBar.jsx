7cac882bb2e5266d14093d9a86e2ed1a000aa13c
Raka Yuda Pradipta <raka.yuda.pradipta@gmail.com>
2024-08-31 19:14:56 +0700

    feat: add datat test id into progress bar

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,7 +1,7 @@
 const ProgressBar = ({height = "h-1", progress = 0}) => {
   return (
     <div className={`mb-6 ${height} w-full bg-gray-100 `}>
-      <div className={`${height} bg-gray-400`} style={{ width: `${progress.toString()}%` }}></div>
+      <div data-testid="progress-bar" className={`${height} bg-gray-400`} style={{ width: `${progress.toString()}%` }}></div>
     </div>
   );
 };
