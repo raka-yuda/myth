db81235b230114ada2cbfd15a7d99bc85f615f14
Raka Yuda Pradipta <raka.yuda.pradipta@gmail.com>
2024-09-01 19:07:14 +0700

    feat: modify trivia card test

--- src/__tests__/components/TriviaCard.test.js
+++ src/__tests__/components/TriviaCard.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
-import '@testing-library/jest-dom';
 import TriviaCard from '@/components/TriviaCard';
 
 describe('TriviaCard', () => {
@@ -8,22 +7,38 @@ describe('TriviaCard', () => {
   const answer = 'Paris';
 
   test('renders the question and flips to show the answer', () => {
-    const { getByText, container } = render(<TriviaCard question={question} answer={answer} />);
+    const onFlipMock = jest.fn();
+    const { getByText, container } = render(
+      <TriviaCard question={question} answer={answer} isFlipped={false} onFlip={onFlipMock} />
+    );
 
     expect(getByText(question)).toBeInTheDocument();
 
     fireEvent.click(getByText(question));
+
+    expect(onFlipMock).toHaveBeenCalledTimes(1);
+  });
+
+  test('applies the "rotate-y-180" class when isFlipped is true', () => {
+    const { container } = render(
+      <TriviaCard question={question} answer={answer} isFlipped={true} onFlip={() => {}} />
+    );
 
     expect(container.firstChild.firstChild).toHaveClass('rotate-y-180');
   });
 
   test('does not flip when highlight is true', () => {
-    const { getByText, container } = render(<TriviaCard question={question} answer={answer} highlight={true} />);
+    const onFlipMock = jest.fn();
+    const { getByText, container } = render(
+      <TriviaCard question={question} answer={answer} highlight={true} isFlipped={false} onFlip={onFlipMock} />
+    );
 
     expect(getByText(question)).toBeInTheDocument();
 
     fireEvent.click(getByText(question));
 
+    expect(onFlipMock).not.toHaveBeenCalled();
+    
     expect(container.firstChild.firstChild).not.toHaveClass('rotate-y-180');
   });
 });
