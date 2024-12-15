d1a32a301f7bdd76000c48ef89edc2ef8a0374e5
Raka Yuda Pradipta <raka.yuda.pradipta@gmail.com>
2024-12-15 22:40:20 +0700

    feat: split card story component

---
+++ src/components/CardStory.jsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+
+const CardStory = ({
+  story,
+  language,
+}) => {
+  return (
+    <div
+      className="rounded-lg shadow p-4 text-black border-8 border-white bg-cover bg-center"
+    >
+      <div className="flex flex-col rounded border-2 border-white p-4 h-full justify-between">
+        <p className="text-xl text-gray-600 mb-24">
+          {story?.title[language]}
+        </p>
+        <p className="text-xl text-gray-600 mb-2">
+          {language === 'english' ? 'Synopsis:' : 'Sinopsis:'}
+        </p>
+        <p className="text-xl text-gray-600 mb-6 line-clamp-5">
+          {story.synopsis[language]}
+        </p>
+        <Link
+          href={`/story/${story.id[language]}`}
+          className="text-base text-end text-gray-600 mb-2 hover:underline"
+        >
+          {language === 'english' ? 'Read More...' : 'Baca Selengkapnya...'} 
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default CardStory;
