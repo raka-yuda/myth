1022beb23f566b6f6d490ee08033c4042bb6c4fc
Raka Yuda Pradipta <raka.yuda.pradipta@gmail.com>
2024-12-18 22:12:06 +0700

    perf: update font import method

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,13 @@ import Loading from "@/components/Loading";
 import "@/styles/globals.css";
 import Head from "next/head";
 import { useEffect, useState } from "react";
+import { Cardo } from '@next/font/google';
+
+const cardo = Cardo({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+});
 
 export default function App({ Component, pageProps }) {
 
@@ -11,7 +18,7 @@ export default function App({ Component, pageProps }) {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
-    }, 1500); 
+    }, 1500);
 
     return () => clearTimeout(timer);
   }, []);
@@ -24,7 +31,7 @@ export default function App({ Component, pageProps }) {
         <meta name="keywords" content="myths, legends, ancient stories, gods, heroes" />
         <meta property="og:title" content="Myth | Explore Ancient Myths" />
         <meta property="og:description" content="Discover and read various ancient myths from around the world. Delve into fascinating legends and more." />
-        <meta property="og:image"  content="https://images.pexels.com/photos/2823459/pexels-photo-2823459.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />
+        <meta property="og:image" content="https://images.pexels.com/photos/2823459/pexels-photo-2823459.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />
         <meta property="og:url" content="https://myth.ryuda.me" />
         <meta name="twitter:card" content="summary_large_image" />
         <script
@@ -48,12 +55,14 @@ export default function App({ Component, pageProps }) {
           }}
         />
       </Head>
-      {(process.env.NODE_ENV === "production") && <Analytics />}
-      {isLoading ? (
-        <Loading text="Welcome to the Mythical World!" duration={1000}/>
-      ) : (
-        <Component {...pageProps} />
-      )}
+      <div className={cardo.className}>
+        {(process.env.NODE_ENV === "production") && <Analytics />}
+        {isLoading ? (
+          <Loading text="Welcome to the Mythical World!" duration={1000} />
+        ) : (
+          <Component {...pageProps} />
+        )}
+      </div>
     </>
   );
 }
