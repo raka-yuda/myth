49b0f7e72ac7954b666cc0b3a766fc17bae6d4a6
Raka Yuda Pradipta <raka.yuda.pradipta@gmail.com>
2025-01-09 14:20:47 +0700

    feat: add tailwind path implement

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/contents/**/*.mdx',
   ],
   theme: {
     extend: {
@@ -32,5 +33,7 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 };
